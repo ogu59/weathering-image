af7acf5656f821806d3a9ff06ad411f163f90d24
ogu59 <595959ogu595959@gmail.com>
2025-08-21 01:21:34 +0000

    Updates

---
+++ precache-manifest.623131e08b395568d787586be709c3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d0324c9f1da57c83c1b093d367c0e3",
+    "url": "/weathering-image/index.html"
+  },
+  {
+    "revision": "61afd46aba050802e512",
+    "url": "/weathering-image/static/css/main.429e7950.chunk.css"
+  },
+  {
+    "revision": "6ef6c22462e74868cf07",
+    "url": "/weathering-image/static/js/2.db2f833a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weathering-image/static/js/2.db2f833a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61afd46aba050802e512",
+    "url": "/weathering-image/static/js/main.56a90cfb.chunk.js"
+  },
+  {
+    "revision": "7e7fe488468e32ca34f0",
+    "url": "/weathering-image/static/js/runtime-main.b05b43a4.js"
+  }
+]);
